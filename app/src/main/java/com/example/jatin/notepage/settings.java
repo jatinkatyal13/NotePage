066867e09f61a87ab6d8f22f663b79f5e3935a1a
Jatin <jatin.katyal13@gmail.com>
2017-02-15 11:41:14 +0530

    #google sign in working

--- app/src/main/java/com/example/jatin/notepage/settings.java
+++ app/src/main/java/com/example/jatin/notepage/settings.java
@@ -1,25 +1,7 @@
 package com.example.jatin.notepage;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.GoogleApiClient;
-
-import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by present on 2/13/17.
@@ -27,59 +9,11 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Settings extends AppCompatActivity{
 
-    private final int RC_SIGN_IN = 9001;
-    private GoogleSignInHandler googleSignInHandler;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        googleSignInHandler = new GoogleSignInHandler(this);
-
-        SignInButton signInButton = (SignInButton)findViewById(R.id.sign_in_button);
-        signInButton.setSize(SignInButton.SIZE_STANDARD);
-        signInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent signInIntent = googleSignInHandler.getSignInIntent();
-                startActivityForResult(signInIntent, RC_SIGN_IN);
-            }
-        });
-
-        if (googleSignInHandler.isSilentLogin()){
-            handleSignInResult(googleSignInHandler.getGoogleSignInResult());
-        } else {
-            ((CardView)findViewById(R.id.profile_card_view)).setVisibility(View.GONE);
-        }
 
     }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        //result returned from launching the intent
-        switch(requestCode){
-            case RC_SIGN_IN:
-                GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-                handleSignInResult(result);
-                break;
-        }
-    }
-
-    private void handleSignInResult(GoogleSignInResult result){
-        if(result.isSuccess()){
-            GoogleSignInAccount account = result.getSignInAccount();
-            ((CardView) findViewById(R.id.sign_in_card)).setVisibility(View.GONE);
-            ((CardView) findViewById(R.id.profile_card_view)).setVisibility(View.VISIBLE);
-            ((TextView) findViewById(R.id.settings_username)).setText(account.getDisplayName());
-            ((TextView) findViewById(R.id.settings_email)).setText(account.getEmail());
-            ((CircleImageView) findViewById(R.id.profile_image)).setImageURI(account.getPhotoUrl());
-
-        } else {
-            Toast.makeText(this, "Sign In Failed", Toast.LENGTH_SHORT).show();
-        }
-    }
-
 }
